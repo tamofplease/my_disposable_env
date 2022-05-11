3a7602aca7c3a6d5c9033946c14cf7a29abdc93c
miwanotomoki <mmiwatomoki@gmail.com>
2022-05-11 14:00:28 +0900

    add several tools to brew.sh files

--- tools/brew.sh
+++ tools/brew.sh
@@ -4,5 +4,8 @@ brew install --cask alfred
 brew install --cask figma
 brew install --cask slack
 brew install --cask zoom
+brew install --cask spotify
 brew install --cask karabiner-elements
+brew install scrcpy
+brew install android-platform-tools
 brew install docker
