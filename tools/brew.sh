a0780b29dea1631f9804cb331735fa6ff9d5d5de
Tomoki Miwa <55216047+tamofplease@users.noreply.github.com>
2023-05-14 01:37:03 +0900

    Update brew.sh

--- tools/brew.sh
+++ tools/brew.sh
@@ -4,7 +4,6 @@ brew install --cask alfred
 brew install --cask figma
 brew install --cask slack
 brew install --cask zoom
-brew install --cask spotify
 brew install --cask karabiner-elements
 brew install --cask docker
 brew install prettier
